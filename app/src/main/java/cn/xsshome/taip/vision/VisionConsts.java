8d1d4445f1532248f553f666739764895c9a9c37
fire2088 <fupengfei8@hotmail.com>
2018-10-29 11:34:56 +0800

    tc_ai_test

---
+++ app/src/main/java/cn/xsshome/taip/vision/VisionConsts.java
@@ -0,0 +1,12 @@
+package cn.xsshome.taip.vision;
+/**
+ * 计算机视觉智能鉴黄、暴恐图片识别 API地址
+ * @author 小帅丶
+ * @version 4.2.0
+ */
+public class VisionConsts {
+	//智能鉴黄
+	static final String VISION_PORN = "https://api.ai.qq.com/fcgi-bin/vision/vision_porn";
+	//暴恐图片识别 
+	static final String IMAGE_TERRORISM = "https://api.ai.qq.com/fcgi-bin/image/image_terrorism";
+}
---
+++ app/src/main/java/com/ai/mexxen/tcaitest/MainActivity.java
@@ -0,0 +1,226 @@
+package com.ai.mexxen.tcaitest;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Looper;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.os.Environment;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.Date;
+import android.os.Message;
+import android.os.Handler;
+
+
+import cn.xsshome.taip.util.FileUtil;
+import cn.xsshome.taip.ocr.TAipOcr;
+import cn.xsshome.taip.nlp.TAipNlp;
+import cn.xsshome.taip.util.ImageDispose;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static android.widget.Toast.*;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mButtonIdDetect, mButtonChat;
+    private TextView mTextView;
+    private EditText mTextChat;
+    public int REQUEST_CODE_TAKE_PICTURE = 1;
+    public Bitmap bm;
+
+    //设置AppID/AppKey
+    public static final String AppID = "your ID";
+    public static final String AppKey = "you AppKey";
+
+    // 初始化一个TAipFace
+    TAipOcr OcrClient = new TAipOcr(AppID,AppKey);
+    TAipNlp NlpClient = new TAipNlp(AppID,AppKey);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        initUI();
+    }
+
+    public void initUI() {
+        mButtonIdDetect = (Button) findViewById(R.id.iddetect);
+        mButtonChat = (Button) findViewById(R.id.chat);
+        mTextView = (TextView)findViewById(R.id.textView);
+        mTextChat = (EditText)findViewById(R.id.editText);
+
+        mButtonIdDetect.setOnClickListener(mButtonListener);
+        mButtonChat.setOnClickListener(mButtonListener);
+        mTextView.setText("This is a test");
+        mTextChat.setText("上海天气");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    String strTemp = "";
+    String strResult = "";
+
+    private Handler  handler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            if (msg.what == 0) {
+                mTextView.setText(strResult); //View.ininvalidate()
+                sendEmptyMessageDelayed(0, 1000);
+            }
+        }
+    };
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 1:
+                if ( resultCode == RESULT_OK) {
+                    bm = (Bitmap) data.getExtras().get("data");
+                    //savePath = FileUtil.saveBitmap(bm);
+                    //img.setImageBitmap(bm);
+                }
+
+                new Thread(new Runnable(){
+                    @Override
+                    public void run() {
+                        try {
+                            strTemp = OcrClient.idcardOcr(ImageDispose.Bitmap2Bytes(bm) , 0);//身份证正面(图片)识别;
+                            try{
+                                String sTemp = "";
+                                JSONObject jsonObject = new JSONObject(strTemp);//jsonArray.getJSONObject(0);
+                                //注意：results中的内容带有中括号[]，所以要转化为JSONArray类型的对象
+                                String result = jsonObject.optString("data");
+                                JSONObject jsonObject1 = new JSONObject(result);
+                                sTemp = sTemp + "Name: " + jsonObject1.optString("name") + "\n";
+                                sTemp = sTemp + "Sex: " + jsonObject1.optString("sex") + "\n";
+                                sTemp = sTemp +  "Nation: " + jsonObject1.optString("nation") + "\n";
+                                sTemp = sTemp +  "Birth: " + jsonObject1.optString("birth") + "\n";
+                                sTemp = sTemp +  "Address: " +  jsonObject1.optString("address") + "\n";
+                                sTemp = sTemp +  "ID: " +  jsonObject1.optString("id");
+                                strResult = sTemp;
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+                while(strResult == "") {
+                    try {
+                        Thread.sleep(200);
+                    } catch (Exception e) {
+
+                    }
+                }
+                handler.sendEmptyMessageDelayed(0, 100);
+                break;
+        }
+    }
+    OnClickListener mButtonListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            mTextView.setText("查询中...");
+            switch (v.getId()) {
+                case R.id.iddetect:
+                    Intent openCameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); //系统常量， 启动相机的关键
+                    startActivityForResult(openCameraIntent, REQUEST_CODE_TAKE_PICTURE); // 参数常量为自定义的request code, 在取返回结果时有用
+                    break;
+                case R.id.chat:
+                    // Android 4.0 之后不能在主线程中请求HTTP请求
+                    new Thread(new Runnable(){
+                        @Override
+                        public void run() {
+                            try {
+                                strTemp = wechat(NlpClient);
+                                try{
+                                    JSONObject jsonObject = new JSONObject(strTemp);//jsonArray.getJSONObject(0);
+                                    //注意：results中的内容带有中括号[]，所以要转化为JSONArray类型的对象
+                                    String result = jsonObject.optString("data");
+                                    JSONObject jsonObject1 = new JSONObject(result);
+                                    strResult = jsonObject1.optString("answer");
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }).start();
+
+                    while(strResult == "") {
+                        try {
+                            Thread.sleep(200);
+                        } catch (Exception e) {
+
+                        }
+                    }
+                    handler.sendEmptyMessageDelayed(0, 100);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    public static String iddetect(TAipOcr client) throws Exception {
+        String result = "";
+
+        /*
+        //参数为本地图片路径
+        String imagePath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/DCIM/Camera/1.jpg";// +"/DCIM/1.jpg";//  /storage/sdcard0/DCIM/1.jpg
+
+        //String result = client.idcardOcr(imagePath, 0);//身份证正面(图片)识别;
+        //String result = client.idcardOcr("./idcard2.jpg", 1);//身份证反面(国徽)识别;
+        //参数为本地图片二进制数组
+
+        byte[] image = FileUtil.readFileByBytes(imagePath);
+        result = client.idcardOcr(image , 0);//身份证正面(图片)识别;
+        //String result = client.idcardOcr(image , 1);//身份证反面(国徽)识别;
+        */
+
+        return result;
+    }
+
+    public String wechat(TAipNlp client) throws Exception {
+        //参数内容
+        String text = mTextChat.getText().toString();
+        String session = new Date().getTime()/1000+"";//会话标识（应用内唯一）
+        //String result = client.nlpTextchat(text);//基础闲聊
+        String result = client.nlpTextchat(session,text);//基础闲聊
+        return result;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
