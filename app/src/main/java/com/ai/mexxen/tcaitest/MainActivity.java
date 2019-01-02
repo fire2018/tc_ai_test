c82d6ccaa39e8cb8a8f232fd61fb784ca8888461
fire2088 <fupengfei8@hotmail.com>
2019-01-02 11:06:27 +0800

    增加通用ocr功能

--- app/src/main/java/com/ai/mexxen/tcaitest/MainActivity.java
+++ app/src/main/java/com/ai/mexxen/tcaitest/MainActivity.java
@@ -36,15 +36,16 @@ import org.json.JSONObject;
 import static android.widget.Toast.*;
 
 public class MainActivity extends AppCompatActivity {
-    private Button mButtonIdDetect, mButtonChat;
+    private Button mButtonIdDetect, mButtonChat,mButtonIdOcr;
     private TextView mTextView;
     private EditText mTextChat;
     public int REQUEST_CODE_TAKE_PICTURE = 1;
     public Bitmap bm;
+    public int iAction = 0;
 
     //设置AppID/AppKey
-    public static final String AppID = "your ID";
-    public static final String AppKey = "you AppKey";
+    public static final String AppID = "2109086611";
+    public static final String AppKey = "dZACwRVu2ZBQ9HBa";
 
     // 初始化一个TAipFace
     TAipOcr OcrClient = new TAipOcr(AppID,AppKey);
@@ -62,10 +63,12 @@ public class MainActivity extends AppCompatActivity {
     public void initUI() {
         mButtonIdDetect = (Button) findViewById(R.id.iddetect);
         mButtonChat = (Button) findViewById(R.id.chat);
+        mButtonIdOcr = (Button)findViewById(R.id.idocr);
         mTextView = (TextView)findViewById(R.id.textView);
         mTextChat = (EditText)findViewById(R.id.editText);
 
         mButtonIdDetect.setOnClickListener(mButtonListener);
+        mButtonIdOcr.setOnClickListener(mButtonListener);
         mButtonChat.setOnClickListener(mButtonListener);
         mTextView.setText("This is a test");
         mTextChat.setText("上海天气");
@@ -85,7 +88,8 @@ public class MainActivity extends AppCompatActivity {
         public void handleMessage(android.os.Message msg) {
             if (msg.what == 0) {
                 mTextView.setText(strResult); //View.ininvalidate()
-                sendEmptyMessageDelayed(0, 1000);
+                mTextChat.setText(strResult);
+                //sendEmptyMessageDelayed(0, 1000);
             }
         }
     };
@@ -102,26 +106,55 @@ public class MainActivity extends AppCompatActivity {
                 new Thread(new Runnable(){
                     @Override
                     public void run() {
-                        try {
-                            strTemp = OcrClient.idcardOcr(ImageDispose.Bitmap2Bytes(bm) , 0);//身份证正面(图片)识别;
-                            try{
-                                String sTemp = "";
-                                JSONObject jsonObject = new JSONObject(strTemp);//jsonArray.getJSONObject(0);
-                                //注意：results中的内容带有中括号[]，所以要转化为JSONArray类型的对象
-                                String result = jsonObject.optString("data");
-                                JSONObject jsonObject1 = new JSONObject(result);
-                                sTemp = sTemp + "Name: " + jsonObject1.optString("name") + "\n";
-                                sTemp = sTemp + "Sex: " + jsonObject1.optString("sex") + "\n";
-                                sTemp = sTemp +  "Nation: " + jsonObject1.optString("nation") + "\n";
-                                sTemp = sTemp +  "Birth: " + jsonObject1.optString("birth") + "\n";
-                                sTemp = sTemp +  "Address: " +  jsonObject1.optString("address") + "\n";
-                                sTemp = sTemp +  "ID: " +  jsonObject1.optString("id");
-                                strResult = sTemp;
-                            } catch (JSONException e) {
+                        //View cv = getWindow().getDecorView();
+                        if(iAction == 1) {
+                            try {
+                                strTemp = OcrClient.idcardOcr(ImageDispose.Bitmap2Bytes(bm), 0);//身份证正面(图片)识别;
+                                try {
+                                    String sTemp = "";
+                                    JSONObject jsonObject = new JSONObject(strTemp);//jsonArray.getJSONObject(0);
+                                    //注意：results中的内容带有中括号[]，所以要转化为JSONArray类型的对象
+                                    String result = jsonObject.optString("data");
+                                    JSONObject jsonObject1 = new JSONObject(result);
+                                    sTemp = sTemp + "Name: " + jsonObject1.optString("name") + "\n";
+                                    sTemp = sTemp + "Sex: " + jsonObject1.optString("sex") + "\n";
+                                    sTemp = sTemp + "Nation: " + jsonObject1.optString("nation") + "\n";
+                                    sTemp = sTemp + "Birth: " + jsonObject1.optString("birth") + "\n";
+                                    sTemp = sTemp + "Address: " + jsonObject1.optString("address") + "\n";
+                                    sTemp = sTemp + "ID: " + jsonObject1.optString("id");
+                                    strResult = sTemp;
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            } catch (Exception e) {
                                 e.printStackTrace();
                             }
-                        } catch (Exception e) {
-                            e.printStackTrace();
+                        }
+                        else if(iAction == 2)
+                        {
+                            try {
+                                strTemp = OcrClient.generalOcr(ImageDispose.Bitmap2Bytes(bm));//ocr
+                                try {
+                                    String sTemp = "";
+                                    JSONObject jsonObject = new JSONObject(strTemp);//jsonArray.getJSONObject(0);
+                                    //注意：results中的内容带有中括号[]，所以要转化为JSONArray类型的对象
+                                    String result = jsonObject.optString("data");
+                                    JSONObject jsonObject1 = new JSONObject(result);
+                                    String result1 =jsonObject1.optString("item_list");
+
+                                    JSONArray jsonArray = new JSONArray(result1);
+                                    int iLength = jsonArray.length();
+                                    for (int i=0; i < jsonArray.length(); i++)    {
+                                        JSONObject jsonObject2 = jsonArray.getJSONObject(i);
+                                        sTemp = sTemp + jsonObject2.getString("itemstring") + "\n";
+                                        strResult = sTemp;
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
                         }
                     }
                 }).start();
@@ -141,10 +174,6 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             mTextView.setText("查询中...");
             switch (v.getId()) {
-                case R.id.iddetect:
-                    Intent openCameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); //系统常量， 启动相机的关键
-                    startActivityForResult(openCameraIntent, REQUEST_CODE_TAKE_PICTURE); // 参数常量为自定义的request code, 在取返回结果时有用
-                    break;
                 case R.id.chat:
                     // Android 4.0 之后不能在主线程中请求HTTP请求
                     new Thread(new Runnable(){
@@ -176,7 +205,13 @@ public class MainActivity extends AppCompatActivity {
                     }
                     handler.sendEmptyMessageDelayed(0, 100);
                     break;
+                case R.id.iddetect:
+                    iAction = 1; //id card
+                case R.id.idocr:
+                    iAction = 2; //common ocr
                 default:
+                    Intent openCameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); //系统常量， 启动相机的关键
+                    startActivityForResult(openCameraIntent, REQUEST_CODE_TAKE_PICTURE); // 参数常量为自定义的request code, 在取返回结果时有用
                     break;
             }
         }
